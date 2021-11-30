22eca1bf749da4087b98db1b92758280dc633423
AroaGO <74042599+aroago@users.noreply.github.com>
2021-12-01 00:58:05 +0100

    30/11/2021 edit login,estilos

--- codigoPHP/loginOut.php
+++ codigoPHP/loginOut.php
@@ -1,105 +1,150 @@
 <?php
+/*
+ * @author: Aroa Granero Omañas
+ * @version: v1
+ * Created on: 30/11/2021
+ * Last modification: 30/11/2021
+ */
+/* Iniciamos la session para alamacenar el codigo de usuario */
+session_start();
+
 //Importamos la libreria de validacion
 require_once '../core/libreriaValidacion.php';
 //Fichero de configuración de la BBDD
 require_once '../config/confDBPDO.php';
-
-$entradaOK = true;
-
-//Array para almacenar los errores del formulario
-$aErrores = [
-    'T01_CodUsuario' => null,
-    'T01_Password' => null
+$aFormulario = [
+    'usuario' => '',
+    'password' => ''
 ];
 
+/**
+ * Si se ha enviado el formulario, valida la entrada.
+ */
+if (isset($_REQUEST['btnlogin'])) {
+    // Manejador de errores. 
+    $bEntradaOK = true;
 
-//Si se ha pulsado enviar
-if (isset($_POST['enviar'])) {
-    //La posición del array de errores recibe el mensaje de error si hubiera
-    $aErrores['T01_CodUsuario'] = validacionFormularios::comprobarAlfabetico($_POST['T01_CodUsuario'], 50, 1, 1);
-    $aErrores['T01_Password'] = validacionFormularios::comprobarAlfaNumerico($_POST['T01_Password'], 20, 1, 1);
-    //Recorre el array en busca de mensajes de error
-    foreach ($aErrores as $campo => $error) {
-        if ($error != null) {
-            //Cambia la condición de la variable
-            $entradaOK = false;
+    
+
+    /** comprueba que el usuario y la contraseña existan y sean correctos en la base de datos.
+     */
+    if ($bEntradaOK) {
+        /* Recogida de información */
+        $aFormulario['usuario'] = $_REQUEST['usuario'];
+        $aFormulario['password'] = $_REQUEST['password'];
+
+        try {
+            /* Establecemos la connection con pdo en global */
+            $miDB = new PDO(HOST, USER, PASSWORD);
+
+            /* configurar las excepcion */
+            $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+               // Query de selección.
+            $sSelect = "SELECT T01_Password FROM T01_Usuario WHERE T01_CodUsuario='{$aFormulario['usuario']}'";
+
+            // Preparación y ejecución de la consulta.
+            $oResultadoSelect = $oDB->prepare($sSelect);
+            $oResultadoSelect->execute();
+
+            $oResultado = $oResultadoSelect->fetchObject();
+        } catch (PDOException $exception) {
+            /*
+             * Mostrado del código de error y su mensaje.
+             */
+            echo '<div>Se han encontrado errores:</div><ul>';
+            echo '<li>' . $exception->getCode() . ' : ' . $exception->getMessage() . '</li>';
+            echo '</ul>';
+        } finally {
+            unset($oDB);
+        }
+
+        if (!$oResultado || $oResultado->T01_Password != hash('sha256', ($aFormulario['usuario'] . $aFormulario['password']))) {
+            $bEntradaOK = false;
         }
     }
-} else {
-    //Cambiamos el valor de la variable porque no se ha pulsado el botón
-    $entradaOK = false;
 }
-if ($entradaOK) {
+/*
+ * Si el formulario no ha sido enviado, pone el manejador de errores
+ * a false para poder mostrar el formulario.
+ */
+else {
+    $bEntradaOK = false;
+}
+
+
+if ($bEntradaOK) {
+    // Variable de sesión para el usuario.
+    $_SESSION['usuarioDAW208AppLoginLogoff'] = $aFormulario['usuario'];
+    
+    // Añadido al registro de conexiones y última hora de conexión.
     try {
-        // Datos de la conexión a la base de datos
-        $mydb = new PDO(HOST, USER, PASSWORD); //Establecer una conexión con la base de datos 
-        $mydb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $codUsuario = $_POST['T01_CodUsuario'];
-        $T01_Password = $_POST['T01_Password'];
-        //Selecciona los datos del usuario que se loguea
-        $consultaSQL = "SELECT * FROM T01_Usuario WHERE T01_CodUsuario = :codigo AND T01_Password = :passHash";
-        $resultadoSQL = $mydb->prepare($consultaSQL);
-        $resultadoSQL->bindValue(':codigo', $T01_CodUsuario);
-        $resultadoSQL->bindValue(':passHash', hash('sha256', $T01_CodUsuario.$T01_Password));
-        $resultadoSQL->execute();
+        // Conexión con la base de datos.
+        $oDB = new PDO(HOST, USER, PASSWORD);
+        $oDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        //Si el resultado del select devuelve algún valor es que el usuario introducido existe
-        if ($resultadoSQL->rowCount() == 1) {
-            $usuario = $resultadoSQL->fetchObject();
-            session_start();
-            $_SESSION['Usuario208DWESLoginLogoutTema5'] = $usuario->T01_CodUsuario;
-            //Actualiza el número de conexiones sumándole 1
-            $conexionesSQL = "UPDATE T01_Usuario SET T01_NumConexiones = T01_NumConexiones + 1 WHERE T01_CodUsuario = :codigo;";
-            $actualizarConexionesSQL = $mydb->prepare($conexionesSQL);
-            $actualizarConexionesSQL->bindParam(":codigo", $_SESSION['Usuario208DWESLoginLogoutTema5']);
-            $actualizarConexionesSQL->execute();
-            //Una vez ejecutadas las sentencias se redirige a programa.php
-            header("Location: progrma.php");
-        } else {
-            //Si el resultado del select no devuelve ningún valor, el usuario no existe, se queda en el login
-            header('Location: loginOut.php');
-        }
-        //Cuando se produce una excepcion se corta el programa y salta la excepción con el mensaje de error
-    } catch (PDOException $mensajeError) {
-        echo "<h3>Mensaje de ERROR</h3>";
-        echo "Error: " . $mensajeError->getMessage() . "<br>";
-        echo "Código de error: " . $mensajeError->getCode();
+        // Fecha-hora actual.
+        $oDateTime = new DateTime();
+
+        // Query de actualización.
+        $sUpdate = <<<QUERY
+                    UPDATE T01_Usuario SET T01_NumConexiones=T01_NumConexiones+1,
+                    T01_FechaHoraUltimaConexion = '{$oDateTime->format("y-m-d h:i:s")}'
+                    WHERE T01_CodUsuario='{$aFormulario['usuario']}'
+            QUERY;
+
+        // Preparación y ejecución de la actualización.
+        $oResultadoUpdate = $oDB->prepare($sUpdate);
+        $oResultadoUpdate->execute();
+    } catch (PDOException $exception) {
+        /*
+         * Mostrado del código de error y su mensaje.
+         */
+        echo '<div>Se han encontrado errores:</div><ul>';
+        echo '<li>' . $exception->getCode() . ' : ' . $exception->getMessage() . '</li>';
+        echo '</ul>';
     } finally {
-        unset($mydb);
+        unset($oDB);
     }
-} else {
-    ?>
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                <fieldset>
-                    <div class="obligatorio">
-                        <strong>Nombre: </strong>
-                        <input type="text" name="T01_CodUsuario" style="border: 1px solid black" 
-                               value="<?php
-                               if ($aErrores['T01_CodUsuario'] == NULL && isset($_POST['T01_CodUsuario'])) {
-                                   echo $_POST['T01_CodUsuario'];
-                               }
-                               ?>">            
-                    </div>
-                    <br>
-                    <div class="obligatorio">
-                        <strong>Contraseña: </strong>
-                        <input type="T01_Password" name="T01_Password" style="border: 1px solid black" 
-                               value="<?php
-                               if ($aErrores['T01_Password'] == NULL && isset($_POST['T01_Password'])) {
-                                   echo $_POST['T01_Password'];
-                               }
-                               ?>">               
-                    </div>
-                    <br>
-                    <div>                
-                        <input type="submit" name="enviar" value="ENTRAR" >
-                    </div>
-                </fieldset>
-            </form>
-        <?php } ?>
 
+    // Reenvío al usuario a la página de programa.
+    header('Location: programa.php');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<!--Aroa Granero Omañas 
+Fecha Creacion: 30/11/2021
+Fecha Modificacion: 30/11/2021 -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="aroaGraneroOmañas">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="application-name" content="ejercicio00 Tema5">
+        <meta name="description" content="variasuper glovales">
+        <meta name="keywords" content=" index, ej00" >
+        <meta name="robots" content="index, follow" />
+        <link rel="shortcut icon" href="favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../webroot/css/estilosEjer.css"  rel="stylesheet"  type="text/css" title="Default style">
+        <title>logIn</title>  
+        
+    </head>
+    <body>
+        <div id="container">
+            <h1>Log In</h1>
+            <span class="close-btn">
+                <img src="https://cdn4.iconfinder.com/data/icons/miu/22/circle_close_delete_-128.png"></img>
+            </span>
+
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="Post">
+                <input type="text" name="usuario" id="username"  placeholder="username">
+                <input type="password" name="password" id="password" placeholder="password">
+                <input type="submit" name="btnlogin" id="btnlogin" value="Entrar">
+               
+            </form>
+        </div>
+        
     </body>
 </html>
